ec8376f036c33c8955935019c20a9d2e8127af95
Azmat Ullah <54760652+azmat06@users.noreply.github.com>
2021-07-04 23:49:57 +0600

    Add files via upload

---
+++ compNewsfeed.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jul 04, 2021 at 07:48 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `compNewsfeed`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`fname`, `lname`, `email`, `mobile`, `username`, `password`) VALUES
+('chris', 'rock', 'chris@gmail.com', '0171', 'chris123', '*676243218923905CF94CB52A3C9D3EB30CE8E20D'),
+('chris', 'rock', 'chris@gmail.com', '0171', 'chris123', '*676243218923905CF94CB52A3C9D3EB30CE8E20D'),
+('hailee', 'atwell', 'atwell@gmail.com', '0191', 'atwell123', '*676243218923905CF94CB52A3C9D3EB30CE8E20D'),
+('hailee', 'atwell', 'atwell@gmail.com', '0191', 'atwell123', '*676243218923905CF94CB52A3C9D3EB30CE8E20D');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
